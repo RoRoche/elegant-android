ff3c8d709afe0595708c786c191f9eb00077562b
Romain Rochegude <romain.rochegude@gmail.com>
2020-02-29 18:08:22 +0100

    deactivate leakcanary

--- app/src/main/java/fr/guddy/elegantandroid/ElegantAndroidApplication.kt
+++ app/src/main/java/fr/guddy/elegantandroid/ElegantAndroidApplication.kt
@@ -1,12 +1,12 @@
 package fr.guddy.elegantandroid
 
 import android.app.Application
-import leakcanary.AppWatcher
+//import leakcanary.AppWatcher
 
 class ElegantAndroidApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        AppWatcher.config = AppWatcher.config.copy(watchFragmentViews = false)
+//        AppWatcher.config = AppWatcher.config.copy(watchFragmentViews = false)
     }
 
     override fun onTerminate() {
