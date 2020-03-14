0880ad1755da9a4722fa842dbb5487407f148960
Romain Rochegude <romain.rochegude@gmail.com>
2020-03-14 19:52:49 +0100

    optimize imports, group queries in a dedicated package in the app

---
+++ app/src/main/java/fr/guddy/elegantandroid/persistence/queries/SelectReposByOwner.kt
@@ -0,0 +1,34 @@
+package fr.guddy.elegantandroid.persistence.queries
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import fr.guddy.elegantandroid.persistence.RepoTable
+import fr.guddy.eoandroidpersistence.Table
+import fr.guddy.eoandroidpersistence.queries.DbQuery
+import fr.guddy.eoandroidpersistence.queries.Select
+import fr.guddy.eoandroidpersistence.queries.Where
+
+class SelectReposByOwner(
+    origin: DbQuery<Cursor>
+) : DbQuery.Wrap<Cursor>(origin) {
+
+    constructor(
+        db: SQLiteDatabase,
+        owner: String,
+        table: Table
+    ) : this(
+        Select(
+            db = db,
+            from = table,
+            where = Where(
+                "owner = ?",
+                arrayOf(owner)
+            )
+        )
+    )
+
+    constructor(
+        db: SQLiteDatabase,
+        owner: String
+    ) : this(db, owner, RepoTable())
+}
