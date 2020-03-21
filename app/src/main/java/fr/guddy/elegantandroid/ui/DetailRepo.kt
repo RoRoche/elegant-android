c2929834308a23a47d7d19f792dc56025c40b63f
Romain Rochegude <romain.rochegude@gmail.com>
2020-03-21 11:36:58 +0100

    document app/ui

--- app/src/main/java/fr/guddy/elegantandroid/ui/DetailRepo.kt
+++ app/src/main/java/fr/guddy/elegantandroid/ui/DetailRepo.kt
@@ -4,9 +4,20 @@ import fr.guddy.elegantandroid.databinding.ActivityRepoBinding
 import fr.guddy.elegantandroid.domain.Repo
 import fr.guddy.eoandroidui.Bindable
 
+/**
+ * Class representing a [Repo] that could be bound to a detail view.
+ *
+ * @param origin The delegate [Repo].
+ */
 class DetailRepo(
     origin: Repo
 ) : Repo.Wrap(origin), Bindable<ActivityRepoBinding> {
+
+    /**
+     * Print object into the [ActivityRepoBinding] parameter.
+     *
+     * @param binding The [ActivityRepoBinding] where to display data.
+     */
     override fun bind(binding: ActivityRepoBinding) {
         binding.name.text = name()
         binding.description.text = description()
