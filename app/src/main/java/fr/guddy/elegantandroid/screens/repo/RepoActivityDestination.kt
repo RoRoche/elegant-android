5d003310846b3577e9843b3dc0c88f0cb1f3cc48
Romain Rochegude <romain.rochegude@gmail.com>
2020-03-21 11:41:37 +0100

    document app/screens

--- app/src/main/java/fr/guddy/elegantandroid/screens/repo/RepoActivityDestination.kt
+++ app/src/main/java/fr/guddy/elegantandroid/screens/repo/RepoActivityDestination.kt
@@ -4,11 +4,24 @@ import android.app.Activity
 import android.content.Intent
 import fr.guddy.eoandroidui.Destination
 
+/**
+ * Class representing the way to go to the [RepoActivity].
+ *
+ * @param delegate The delegate [Destination].
+ */
 class RepoActivityDestination(
     delegate: Destination
 ) : Destination.Wrap(
     delegate = delegate
 ) {
+
+    /**
+     * Secondary constructor that builds a [Destination.ToActivity] to go to [RepoActivity] with
+     * proper [RepoActivityExtra].
+     *
+     * @param origin The [Activity] that wants to go to [RepoActivity].
+     * @param repoId The ID of the repo to be displayed.
+     */
     constructor(
         origin: Activity,
         repoId: Long
--- app/src/main/java/fr/guddy/elegantandroid/screens/repos/OnRepoClickListener.kt
+++ app/src/main/java/fr/guddy/elegantandroid/screens/repos/OnRepoClickListener.kt
@@ -2,6 +2,15 @@ package fr.guddy.elegantandroid.screens.repos
 
 import fr.guddy.elegantandroid.domain.Repo
 
+/**
+ * Listener to be called when a repo is clicked in a list.
+ */
 interface OnRepoClickListener {
+
+    /**
+     * Method called when a repos is clicked in a list.
+     *
+     * @param repo The clicked [Repo].
+     */
     fun onClickRepo(repo: Repo)
 }
