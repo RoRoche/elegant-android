9bca8e56ba71d45b0c90cbfa94a2dc6da0c8b86f
Romain Rochegude <romain.rochegude@gmail.com>
2020-03-21 11:24:29 +0100

    document app/application

--- app/src/main/java/fr/guddy/elegantandroid/ElegantAndroidApplication.kt
+++ app/src/main/java/fr/guddy/elegantandroid/ElegantAndroidApplication.kt
@@ -4,13 +4,16 @@ import android.app.Application
 
 //import leakcanary.AppWatcher
 
+/**
+ * The class representing the [Application].
+ */
 class ElegantAndroidApplication : Application() {
+
+    /**
+     * Called when the application is starting
+     */
     override fun onCreate() {
         super.onCreate()
 //        AppWatcher.config = AppWatcher.config.copy(watchFragmentViews = false)
-    }
-
-    override fun onTerminate() {
-        super.onTerminate()
     }
 }
