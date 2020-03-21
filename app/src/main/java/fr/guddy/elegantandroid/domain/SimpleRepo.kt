7fe55a3f347ca91997b05ef834399997d5f5443b
Romain Rochegude <romain.rochegude@gmail.com>
2020-03-21 11:10:24 +0100

    document app/domain and app/json

--- app/src/main/java/fr/guddy/elegantandroid/domain/SimpleRepo.kt
+++ app/src/main/java/fr/guddy/elegantandroid/domain/SimpleRepo.kt
@@ -1,5 +1,14 @@
 package fr.guddy.elegantandroid.domain
 
+/**
+ * A simple implementation of the [Repo] interface.
+ *
+ * @property id The repo's ID.
+ * @property name The repo's ID.
+ * @property description The repo's ID.
+ * @property url The repo's ID.
+ * @property owner The repo's ID.
+ */
 class SimpleRepo(
     private val id: Long,
     private val name: String,
@@ -7,13 +16,29 @@ class SimpleRepo(
     private val url: String,
     private val owner: String
 ) : Repo {
+
+    /**
+     * @return The repo's ID.
+     */
     override fun id() = this.id
 
+    /**
+     * @return The repo's name.
+     */
     override fun name() = this.name
 
+    /**
+     * @return The repo's description.
+     */
     override fun description() = this.description
 
+    /**
+     * @return The repo's URL.
+     */
     override fun url() = this.url
 
+    /**
+     * @return The repo's owner.
+     */
     override fun owner() = this.owner
 }
--- app/src/main/java/fr/guddy/elegantandroid/json/JsonRepos.kt
+++ app/src/main/java/fr/guddy/elegantandroid/json/JsonRepos.kt
@@ -5,17 +5,46 @@ import fr.guddy.eorest.RestRequest
 import okhttp3.Response
 import org.json.JSONArray
 
-class JsonRepos(private val repos: List<Repo>) : List<Repo> by repos {
+/**
+ * Class representing a list of [Repo] in JSON.
+ *
+ * @property repos The [List] of repos to use as a delegate.
+ */
+class JsonRepos(
+    private val repos: List<Repo>
+) : List<Repo> by repos {
+
+    /**
+     * Secondary constructor to build [JSONArray] from a raw [String].
+     *
+     * @param json The raw [String] containing JSON data.
+     */
     constructor(json: String) : this(JSONArray(json))
 
+    /**
+     * Secondary constructor to deal with a [JSONArray] containing the repos.
+     *
+     * @param jsonArray [JSONArray] containing the repos.
+     */
     constructor(jsonArray: JSONArray) : this(
         (0 until jsonArray.length())
             .map(jsonArray::getJSONObject)
             .map { JsonRepo(it) }
     )
 
-    constructor(restRequest: RestRequest) : this(restRequest.response())
+    /**
+     * Secondary constructor that performs a [RestRequest] to get repos in JSON.
+     *
+     * @param restRequest The [RestRequest] to perform.
+     */
+    constructor(
+        restRequest: RestRequest
+    ) : this(restRequest.response())
 
+    /**
+     * Secondary constructor that deals with [Response]
+     * to get a raw string containing repos as JSON.
+     */
     constructor(response: Response) : this(
         response.body!!.string()
     )
