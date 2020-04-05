283e3d3bf5e66a1f4331e12fb9a030941e160cf9
Romain Rochegude <romain.rochegude@gmail.com>
2020-04-05 18:16:02 +0200

    test RestRequest with OkHttp.Call

--- EoRest/src/test/java/fr/guddy/eorest/requests/WithOkHttpRestRequestTest.kt
+++ EoRest/src/test/java/fr/guddy/eorest/requests/WithOkHttpRestRequestTest.kt
@@ -1,3 +1,42 @@
 package fr.guddy.eorest.requests
 
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.amshove.kluent.shouldBe
+import org.junit.After
+import org.junit.Test
 
+/**
+ * Tests for [RestRequest.WithOkHttp] class.
+ */
+class WithOkHttpRestRequestTest {
+
+    var server: MockWebServer = MockWebServer()
+
+    @After
+    fun afterEach() {
+        server.shutdown()
+    }
+
+    @Test
+    fun `test that Call is used to fetch a response`() {
+        server.enqueue(MockResponse().setBody("Hello World!"))
+        server.start()
+        val baseUrl: HttpUrl = server.url("/v1/greetings")
+        RestRequest.WithOkHttp(
+            client = OkHttpClient(),
+            request = Request.Builder()
+                .url(
+                    baseUrl.newBuilder()
+                        .addPathSegment("v1")
+                        .addPathSegment("greetings")
+                        .build()
+                )
+                .build()
+        ).response().body!!.string().shouldBe("Hello World!")
+        server.takeRequest().path!!.shouldBe("/v1/greetings")
+    }
+}
