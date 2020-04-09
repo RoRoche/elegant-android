774e0b0a2de03c34b6fce8203664f0d50e7dea3a
Romain Rochegude <romain.rochegude@gmail.com>
2020-04-09 22:18:24 +0200

    fix tests

--- eoandroidconcurrency/src/androidTest/java/fr/guddy/eoandroidconcurrency/CallbackTest.kt
+++ eoandroidconcurrency/src/androidTest/java/fr/guddy/eoandroidconcurrency/CallbackTest.kt
@@ -1,24 +1,33 @@
 package fr.guddy.eoandroidconcurrency
 
 import android.os.Looper
-import org.amshove.kluent.shouldBe
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
 
 /**
  * Tests of the [Callback] interface.
  */
 class CallbackTest {
 
-    private class ThreadedCallback : Callback<Int> {
+    private class ThreadedCallback(
+        countDownLatch: CountDownLatch
+    ) : Callback.WithCountDownLatch<Int>(countDownLatch) {
+        private var data: Int = 0
         private lateinit var thread: Thread
 
         override fun accept(data: Int) {
+            this.data = data
             this.thread = Thread.currentThread()
+            super.accept(data)
         }
 
         override fun dispose() {
             // does nothing
         }
+
+        fun data() = this.data
 
         fun thread() = this.thread
     }
@@ -28,12 +37,23 @@ class CallbackTest {
      */
     @Test
     fun testThatCallbackOnMaiThreadCalledOnMainThread() {
-        val threadedCallback = ThreadedCallback()
+        val countDownLatch = CountDownLatch(1)
+        val threadedCallback = ThreadedCallback(countDownLatch)
         Callback.OnMainThread(
             delegate = threadedCallback
         ).accept(42)
-        threadedCallback.thread().shouldBe(
-            Looper.getMainLooper().thread
+        countDownLatch.await()
+        assertThat(
+            threadedCallback.data(),
+            equalTo(
+                42
+            )
+        )
+        assertThat(
+            threadedCallback.thread(),
+            equalTo(
+                Looper.getMainLooper().thread
+            )
         )
     }
 }
