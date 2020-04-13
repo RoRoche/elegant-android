04d4b286504f8cf135525fb8380370224c6c2a5a
Romain Rochegude <romain.rochegude@gmail.com>
2020-04-13 21:34:41 +0200

    add update query

---
+++ eoandroidpersistence/src/main/java/fr/guddy/eoandroidpersistence/queries/Update.kt
@@ -0,0 +1,54 @@
+package fr.guddy.eoandroidpersistence.queries
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import fr.guddy.eoandroidpersistence.Persistable
+import fr.guddy.eoandroidpersistence.Table
+
+/**
+ * [DbQuery] to update data in database.
+ *
+ * @property db The database where to insert data.
+ * @property table The table where to insert data.
+ * @property values The values to insert.
+ * @property where Optional where clause.
+ */
+class Update(
+    private val db: SQLiteDatabase,
+    private val table: Table,
+    private val values: ContentValues,
+    private val where: Where? = null
+) : DbQuery<Int> {
+
+    /**
+     * Secondary constructor to update data from a [Persistable] object.
+     *
+     * @param db The database where to insert data.
+     * @param into The table where to insert data.
+     * @param persistable The [Persistable] object to insert.
+     * @param where Optional where clause.
+     */
+    constructor(
+        db: SQLiteDatabase,
+        into: Table,
+        persistable: Persistable,
+        where: Where? = null
+    ) : this(
+        db,
+        into,
+        persistable.toContentValues(),
+        where
+    )
+
+    /**
+     * @return The number of rows updated.
+     */
+    override fun result(): Int {
+        return db.update(
+            table.name(),
+            values,
+            where?.selection,
+            where?.args
+        )
+    }
+}
