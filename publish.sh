82a843bf3c8b5348d62e6b7d05f3da870a95114c
Romain Rochegude <romain.rochegude@gmail.com>
2020-04-26 21:19:30 +0200

    configure closeAndRelease

--- publish.sh
+++ publish.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#echo $GPG_KEY_CONTENTS | base64 -d > ./secret.gpg
-./gradlew eoandroidconcurrency:assemble
-./gradlew eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
-#./gradlew eorest:jar
-#./gradlew eorest:publishReleasePublicationToSonatypeRepository
-#./gradlew closeAndReleaseRepository
+#echo $GPG_KEY_CONTENTS | base64 -d > /secret.gpg
+./gradlew -PnewVersion=0.0.3 eoandroidconcurrency:clean
+./gradlew -PnewVersion=0.0.3 eoandroidconcurrency:assemble
+./gradlew -PnewVersion=0.0.3 eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
+./gradlew closeAndReleaseRepository
+#./gradlew eoandroidworkmanager:clean
+#./gradlew eoandroidworkmanager:assemble
+#./gradlew eoandroidworkmanager:publishReleasePublicationToSonatypeRepository
+./gradlew -PnewVersion=0.0.3 eorest:clean
+./gradlew -PnewVersion=0.0.3 eorest:jar
+./gradlew -PnewVersion=0.0.3 eorest:publishReleasePublicationToSonatypeRepository
+./gradlew closeAndReleaseRepository
