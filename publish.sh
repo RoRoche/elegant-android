b8ad5cae65014215c7576aceb71e0d00413ad68f
Romain Rochegude <romain.rochegude@gmail.com>
2020-04-27 12:23:42 +0200

    fix javadoc and checksum for CI/CD

--- publish.sh
+++ publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #echo $GPG_KEY_CONTENTS | base64 -d > /secret.gpg
-./gradlew -PnewVersion=0.0.3 eoandroidconcurrency:clean
-./gradlew -PnewVersion=0.0.3 eoandroidconcurrency:assemble
-./gradlew -PnewVersion=0.0.3 eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
+./gradlew -PnewVersion=0.0.2 eoandroidconcurrency:clean
+./gradlew -PnewVersion=0.0.2 eoandroidconcurrency:assemble
+./gradlew -PnewVersion=0.0.2 eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
 ./gradlew closeAndReleaseRepository
 #./gradlew eoandroidworkmanager:clean
 #./gradlew eoandroidworkmanager:assemble
 #./gradlew eoandroidworkmanager:publishReleasePublicationToSonatypeRepository
-./gradlew -PnewVersion=0.0.3 eorest:clean
-./gradlew -PnewVersion=0.0.3 eorest:jar
-./gradlew -PnewVersion=0.0.3 eorest:publishReleasePublicationToSonatypeRepository
+./gradlew -PnewVersion=0.0.2 eorest:clean
+./gradlew -PnewVersion=0.0.2 eorest:jar
+./gradlew -PnewVersion=0.0.2 eorest:publishReleasePublicationToSonatypeRepository
 ./gradlew closeAndReleaseRepository
