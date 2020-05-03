afe9c40409336739fb54def4be421d01d4792d5e
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-04 01:39:34 +0200

    follow io.codearte.nexus-staging recommandations

--- publish.sh
+++ publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #echo $GPG_KEY_CONTENTS | base64 -d > /secret.gpg
-./gradlew -PnewVersion=0.0.2 eoandroidconcurrency:clean
-./gradlew -PnewVersion=0.0.2 eoandroidconcurrency:assemble
-./gradlew -PnewVersion=0.0.2 eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
+./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:clean
+./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:assemble
+./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
 #./gradlew closeAndReleaseRepository
 #./gradlew eoandroidworkmanager:clean
 #./gradlew eoandroidworkmanager:assemble
 #./gradlew eoandroidworkmanager:publishReleasePublicationToSonatypeRepository
-./gradlew -PnewVersion=0.0.2 eorest:clean
-./gradlew -PnewVersion=0.0.2 eorest:jar
-./gradlew -PnewVersion=0.0.2 eorest:publishReleasePublicationToSonatypeRepository
-./gradlew closeAndReleaseRepository
+./gradlew -PnewVersion=0.0.4 eorest:clean
+./gradlew -PnewVersion=0.0.4 eorest:jar
+./gradlew -PnewVersion=0.0.4 eorest:publishReleasePublicationToSonatypeRepository
+./gradlew publish closeAndReleaseRepository
