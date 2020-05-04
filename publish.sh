39b5804f78568cfed012141ebb047e51efc4167c
Romain Rochegude <romain.rochegude@gmail.com>
2020-05-04 02:18:04 +0200

    remove useless logs

--- publish.sh
+++ publish.sh
@@ -3,12 +3,10 @@
 #echo $GPG_KEY_CONTENTS | base64 -d > /secret.gpg
 ./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:clean
 ./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:assemble
-./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:publishReleasePublicationToSonatypeRepository
-#./gradlew closeAndReleaseRepository
-#./gradlew eoandroidworkmanager:clean
-#./gradlew eoandroidworkmanager:assemble
-#./gradlew eoandroidworkmanager:publishReleasePublicationToSonatypeRepository
+./gradlew -PnewVersion=0.0.4 eoandroidconcurrency:publish eoandroidconcurrency:closeAndReleaseRepository
+#./gradlew -PnewVersion=0.0.4 eoandroidworkmanager:clean
+#./gradlew -PnewVersion=0.0.4 eoandroidworkmanager:assemble
+#./gradlew -PnewVersion=0.0.4 eoandroidworkmanager:publish eoandroidworkmanager:closeAndReleaseRepository
 ./gradlew -PnewVersion=0.0.4 eorest:clean
 ./gradlew -PnewVersion=0.0.4 eorest:jar
-./gradlew -PnewVersion=0.0.4 eorest:publishReleasePublicationToSonatypeRepository
-./gradlew publish closeAndReleaseRepository
+./gradlew -PnewVersion=0.0.4 eorest:publish eorest:closeAndReleaseRepository
